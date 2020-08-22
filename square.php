4e142dd1805b6d1ab42fd7a7baf490b46161a124
Tushar0811 <47660783+Tushar0811@users.noreply.github.com>
2020-08-22 14:36:57 +0530

    area using php

---
+++ square.php
@@ -0,0 +1,9 @@
+<?php
+$length = 14;
+$breadth= 10;
+$radius = 7;
+$side = 14;
+echo "The area of the rectangle having dimenions" .$length. " and " .$breadth. " is ". $length*$breadth."."."<br><br><br>";
+echo "The area of the circle having radius ".$radius." is ". 3.14*$radius*$radius. ".<br><br><br>";
+echo "The area of the square having side ".$side." is ".$side*$side.".<br><br><br>";
+?>
